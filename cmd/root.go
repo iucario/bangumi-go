813f2785c0a380886d132181d0fd5d096db62f5e
iucario <16816842+iucario@users.noreply.github.com>
2025-06-06 21:28:53 +0900

    defaults to ui command if no command provided

--- cmd/root.go
+++ cmd/root.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/iucario/bangumi-go/util"
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 )
 
 var ConfigDir string
@@ -14,12 +15,18 @@ var RootCmd = &cobra.Command{
 	Use:   "bgm",
 	Short: "bgm is a command line tool for Bangumi.tv",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Bangumi CLI")
-		fmt.Println("Use 'bgm help' for more information.")
+
 	},
 }
 
 func Execute() {
+	cmd, _, err := RootCmd.Find(os.Args[1:])
+	// default cmd if no cmd is given
+	if err == nil && cmd.Use == RootCmd.Use && cmd.Flags().Parse(os.Args[1:]) != pflag.ErrHelp {
+		args := append([]string{"ui"}, os.Args[1:]...)
+		RootCmd.SetArgs(args)
+	}
+
 	if err := RootCmd.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
@@ -27,14 +34,5 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.play.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 	ConfigDir = util.ConfigDir()
 }
