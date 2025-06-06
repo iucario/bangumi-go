88b50508fc66c3ab92501b6247660db310b673cd
iucario <16816842+iucario@users.noreply.github.com>
2025-06-06 10:46:16 +0900

    add header connection: close

--- cmd/auth/login.go
+++ cmd/auth/login.go
@@ -2,6 +2,7 @@ package auth
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
 	"log/slog"
@@ -15,9 +16,11 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var port = 9090
+
 var loginCmd = &cobra.Command{
 	Use:   "login",
-	Short: "Login to https://bangumi.tv",
+	Short: "Login to https://bgm.tv (bangumi.tv)",
 	Run: func(cmd *cobra.Command, args []string) {
 		_, err := api.LoadCredential()
 		if err != nil {
@@ -35,66 +38,57 @@ var loginCmd = &cobra.Command{
 }
 
 func BrowserLogin() {
-	fmt.Println("Login to https://bangumi.tv")
+	fmt.Println("Login to https://bgm.tv")
 	apiAuth := "https://bgm.tv/oauth/authorize"
 	responseType := "code"
-	host := "http://localhost:9090/auth"
+	host := fmt.Sprintf("http://localhost:%d/auth", port)
 	LOGIN_URL := fmt.Sprintf("%s?client_id=%s&response_type=%s&redirect_uri=%s", apiAuth, api.ClientId, responseType, host)
+
+	serverDone := &sync.WaitGroup{}
+	serverDone.Add(1)
+	start(serverDone)
 
 	openBrowser(LOGIN_URL)
 	fmt.Println("If your browser is not opened automatically. Manually open this URL in browser and login:")
 	fmt.Println(LOGIN_URL)
 
-	serverDone := &sync.WaitGroup{}
-	serverDone.Add(1)
-	start(context.Background(), serverDone)
 	serverDone.Wait()
 }
 
-func start(ctx context.Context, wg *sync.WaitGroup) {
-	srv := &http.Server{Addr: ":9090"}
-	mux := http.NewServeMux()
-	mux.HandleFunc("/auth", func(w http.ResponseWriter, r *http.Request) {
-		select {
-		case <-ctx.Done():
-			fmt.Println("Context Shuting down ...")
-			return
-		default:
-		}
+func start(wg *sync.WaitGroup) {
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", port),
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  120 * time.Second,
+	}
+
+	http.HandleFunc("/auth", func(w http.ResponseWriter, r *http.Request) {
 		code := r.URL.Query().Get("code")
 		if code != "" {
 			Client.GetAccessToken(code)
-			// flush response before shutdown
-			w.WriteHeader(http.StatusOK)
-			_, err := w.Write([]byte("Login success. You can close this page now."))
-			if err != nil {
-				slog.Error("response", "error", err)
-			}
-			flusher, ok := w.(http.Flusher)
-			if ok {
-				flusher.Flush()
-			}
 			fmt.Println("Login success.")
+			w.Header().Set("Connection", "close")
+			w.Header().Set("Content-Type", "text/html; charset=utf-8")
+			fmt.Fprintln(w, "Login success. You can close this page now.")
 			// Give browser time to render response before shutdown
 			go func() {
-				time.Sleep(1 * time.Second)
-				if err := srv.Shutdown(context.Background()); err != nil {
-					slog.Info("server.Shutdown:", "Error", err)
+				ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+				defer cancel()
+				if err := srv.Shutdown(ctx); err != nil {
+					slog.Error("Server Shutdown", "error", err)
 				}
 			}()
 		} else {
-			_, err := fmt.Fprintln(w, "Hi") // Server HTML page to fetch token and return to server at /auth
-			if err != nil {
-				slog.Error("Error writing response", "Error", err)
-			}
+			w.Header().Set("Content-Type", "text/html; charset=utf-8")
+			fmt.Fprintln(w, "Hi")
 		}
 	})
-	srv.Handler = mux
 
 	go func() {
 		defer wg.Done()
-		if err := srv.ListenAndServe(); err != http.ErrServerClosed {
-			slog.Error(fmt.Sprintf("ListenAndServe(): %v", err))
+		if err := srv.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
+			slog.Error("ListenAndServe", "error", err)
 		}
 	}()
 }
