d77ff82fa847bef39c93ed2e49e8b2d03a1983f5
iucario <16816842+iucario@users.noreply.github.com>
2025-06-13 00:13:32 +0900

    a little help page style

--- cmd/ui/help.go
+++ cmd/ui/help.go
@@ -1,7 +1,10 @@
 package tui
 
 import (
+	"fmt"
+
 	"github.com/gdamore/tcell/v2"
+	"github.com/iucario/bangumi-go/internal/ui"
 	"github.com/rivo/tview"
 )
 
@@ -11,42 +14,35 @@ type Help struct {
 }
 
 func NewHelpPage(app *App) *Help {
-	text := `Welcome to Bangumi TUI
-	<https://github.com/iucario/bangumi-go>
+	text := `
+    Welcome to Bangumi TUI
+    <https://github.com/iucario/bangumi-go>
 
-	Shortcuts:
+    Shortcuts:
 
-	[General]
-	1: Go to watching list
-	2: Go to wish list
-	3: Go to done list
-	4: Go to stashed list
-	5: Go to dropped list
-	6: Go to calendar
-	7: Go to search (not yet)
-	0: Go to user info (not yet)
-	?: Show this help
+    [%s]Pages[-]                               [%s]General[-]
+    1: Go to watching list              e: Edit collection
+    2: Go to wish list                  R: Refresh list
+    3: Go to done list                  n: Load next page
+    4: Go to stashed list               Space/Enter: View subject
+    5: Go to dropped list               
+    6: Go to calendar
+    7: Go to search (not yet)           
+    0: Go to user info (not yet)
+    ?: Show this help
 
-	j/up: Move up
-	k/down: Move down
-	h/left: Switch to left
-	l/right: Switch to right
-	Q: Quit
-	q/Esc: Back
-
-	[Collection List]
-	e: Edit collection
-	R: Refresh list
-	n: Load next page
-	Space/Enter: View subject
-
-	[Subject Page]
-	e: Edit collection
-
-	[Calendar]
-	Space/Enter: View subject
-	`
-	view := tview.NewTextView().SetText(text)
+    [%s]Navigation[-]
+    j/up: Move up
+    k/down: Move down
+    h/left: Switch to left
+    l/right: Switch to right
+    Q: Quit
+    q/Esc: Back
+    `
+	color := ui.ColorToHex(ui.Styles.TertiaryTextColor)
+	colorText := fmt.Sprintf(text, color, color, color)
+	view := tview.NewTextView().SetText(colorText).SetDynamicColors(true)
+	view.SetWrap(false)
 
 	view.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
 		if event.Key() == tcell.KeyRune {
